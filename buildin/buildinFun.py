0aad7023e3719e4e618253419224bf26b65b0b51
YONGXI YANG <reganyang0415@gamil.com>
2019-12-25 15:29:17 +0800

    update

---
+++ buildin/buildinFun.py
@@ -0,0 +1,38 @@
+#%%
+# python 有很多内嵌的函数， 先来 介绍一下
+
+#%%[markdown]
+# print 函数 在终端控制台打印
+# ```
+# def print(*values: object, \
+#      sep: Text=..., end: Text=..., file: Optional[_Writer]=..., flush: bool=...)
+# ```
+
+#%%
+a = 123
+b = "yyx"
+print(a)
+print(a, b)
+print(a, b, (1, 2, 3))
+print(a, b, end="\n\n")
+print(a, b, sep="\t")
+# with open("kk.txt", "w") as txtfile:
+#     print(a, b, file=txtfile)
+# %%
+# type, isinstance 是判断 变量类型的, isinstance 比较推荐用
+a = "123"
+b = 123
+print(f"the type of a is {type(a)}")
+print(f"the type of b is {type(b)}")
+print(f"the type of a is str {isinstance(a, str)}")
+print(f"the type of b is int {isinstance(b, int)}")
+
+#%%
+# max 取最大值， min 最小值， len 长度， abs 绝对值
+
+#%%
+a = [1, 2, 3, -1]
+print(f"the max value of a is {max(a)}")
+print(f"the min value of a is {min(a)}")
+print(f"the len value of a is {len(a)}")
+print(f"the aboslute value is {abs(-1)}")
---
+++ datatype/datatype.py
@@ -0,0 +1,42 @@
+#%%
+# python 有几种数据类型
+# 1. 整数型 int
+# 2. 浮点型 float
+# 3. 元组 tuple
+# 4. 字典 dictionary
+# 5. 列表 list
+# 6. 字符类型 str
+# 7. 集合类型 set
+# %%
+a = 3 #整数型
+print(a)
+print(f"the type of variable a is {'int' if isinstance(a, int) == True else 'unknow'}")
+
+# %%
+a = 3.14
+print(a)
+print(f"the type of variable a is {'float' if isinstance(a, float) == True else 'unknow'}")
+
+# %%
+a = (1, 2, 3)
+print(a)
+print(f"the type of variable a is {'tuple' if isinstance(a, tuple) == True else 'unknow'}")
+
+# %%
+a = [1, 2, 3]
+print(a)
+print(f"the type of variable a is {'list' if isinstance(a, list) == True else 'unknow'}")
+
+#%%
+a = "'123'"
+print(a)
+print(f"the type of variable a is {'str' if isinstance(a, str) == True else 'unknow'}")
+
+#%%
+a = {
+    "yang": 0, 
+    "yong": 1,
+    "xi": 2
+}
+print(a)
+print(f"the type of variable a is {'dict' if isinstance(a, dict) == True else 'unknow'}")
